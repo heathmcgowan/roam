9adf6d98eb7c1c348de5512e1801490a4dfed93c
heathmcgowan <heathjmcgowan@gmail.com>
2019-04-09 18:27:53 +1200

    added project details on thumbnail hover and altered overall css

--- js/main.js
+++ js/main.js
@@ -1,31 +1,55 @@
 // Global Variables
+var numberInArray = 1;
 let headerHeight = $('header').height();
 let designerCardHeight = $('.designer-card').height();
 let triggerHeight = designerCardHeight + 75;
-const behanceKey = '40CPyv6Gz9Kny0Hl2vwjYBhbGM2zdplV';
+const behanceKey = 'SCJnOBwjJqgpwxIybOHvs0cUt0XRrydH';
+// const behanceKey = '40CPyv6Gz9Kny0Hl2vwjYBhbGM2zdplV';
 let designer = [
     {
-        bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur.",
-        userID: "CourtneyTyler"
+        bio: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur.',
+        userID: 'CourtneyTyler'
     },
     {
-        bio: "Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam?",
-        userID: "otvazhniy"
+        bio: 'Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam?',
+        userID: 'otvazhniy'
     },
     {
-        bio: "Lorem ipsum dolor sit, amet consectetur adipisiasfa sfa sfcing elit. Accusamua sfa s illum suscipit dolore ma sfinima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur.",
-        userID: "fatihocak"
+        bio: 'Lorem ipsum dolor sit, amet consectetur adipisiasfa sfa sfcing elit. Accusamua sfa s illum suscipit dolore ma sfinima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur.',
+        userID: 'fatihocak'
     },
     {
-        bio: "Accusamus illum suscipitas fa dolorasf asf minima consequatur ex qu asfasi, easfa s avenieta sf facere aut. Placeat minima asf asrerum debitis eaque neque mollitia quia facere laudantium numquam?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam?",
-        userID: "nerokore"
+        bio: 'Accusamus illum suscipitas fa dolorasf asf minima consequatur ex qu asfasi, easfa s avenieta sf facere aut. Placeat minima asf asrerum debitis eaque neque mollitia quia facere laudantium numquam?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus illum suscipit dolore minima consequatur ex quasi, eveniet facere aut. Placeat minima rerum debitis eaque neque mollitia quia facere laudantium numquam?',
+        userID: 'nerokore'
     }
 ]
+boris = {
+    name: 'Boris',
+    image: '../img/boris.jpg',
+    title: 'CEO',
+    bio: "Boris is the fat cat in charge. He sleeps all day while his team does all the hard work. There's no pussyfooting around it, he expects only the best, so don't rub him the wrong way. Get everything purrfect and he'll think you're the cat's whiskers.",
+    userID: 'nerokore'
+}
 
-// Loader
 $(document).ready(function () {
+    $(document).scrollTop(0);
     updateDesignerArray();
-    $(this).scrollTop(0);
+    designer.push(boris);
+    checkArrayLoaded();
+});
+
+// Check Designer's details pulled from Behance correctly before removing loader
+function checkArrayLoaded() {
+    if (numberInArray < designer.length) {
+        window.setTimeout(checkArrayLoaded, 100);
+    } else if (numberInArray === designer.length) {
+        removeLoader();
+    }
+}
+
+// Remove loader
+function removeLoader() {
+    console.log('done');
     setTimeout(function() {
         $('.loader').addClass('hidden-opacity');
     }, 1500); // TODO: Change back to 1500
@@ -34,11 +58,11 @@ $(document).ready(function () {
         loadHero();
         loadDesigners();
     }, 2000); // TODO: Change back to 2000
-});
+}
 
-// Load hero content
+// Show hero content
 function loadHero() {
-    $('.hero-content').append('<object type="image/svg+xml" data="img/teamillustration-animated.svg"></object>');
+    $('.hero-content').append('<object type="image/svg+xml" id="team-illustration" data="img/teamillustration-animated.svg"></object>');
     setTimeout(function() {
         $('.background-triangle').removeClass('hidden-opacity');
     }, 200);
@@ -51,6 +75,7 @@ function loadHero() {
     }, 2200);
 }
 
+// Insert designer cards
 function loadDesigners() {
     for (let i = 0; i < designer.length; i++) {  // TODO: change to handlebars
         $('main').append(`<div class="designer-card dc${i}"><div class="designer-card-top"><div class="designer-details"><h3>${designer[i].name}</h3><h4>${designer[i].title}</h4><p>${designer[i].bio}</p><button class="projects-button pointer" data-index="${i}">View Projects</button></div><div class="designer-image"><img src="${designer[i].image}" alt="${designer[i].name}'s Profile Photo"></div></div><div class="designer-card-bottom projects-closed dcb${i}"><ul class="projects projects${i}"></ul></div></div>`);
@@ -72,7 +97,6 @@ function loadDesigners() {
     onResize();
 }
 
-// Scroll functions
 // Recalculate heights on window resize
 function onResize() {
     headerHeight = $('header').height();
@@ -96,7 +120,7 @@ $(window).scroll(function () {
         $('.scroll-hero').removeClass('hidden-opacity');
     }
     // TODO: Update loop with array details
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < designer.length; i++) {
         if ($(this).scrollTop() > ((headerHeight / 2) + triggerHeight * i)) {
             $(`.dc${i}`).addClass('on-screen');
         } else {
@@ -114,13 +138,15 @@ function updateDesignerArray() {
             url: urlProjects,
             dataType: 'jsonp',
             success: function(res) { // eslint-disable-line
-            designer[currentUser].name = `${res.user.first_name} ${res.user.last_name}`;
-            designer[currentUser].image = res.user.images['276'];
-            designer[currentUser].title = res.user.occupation;
+                designer[currentUser].name = `${res.user.first_name} ${res.user.last_name}`;
+                designer[currentUser].image = res.user.images['276'];
+                designer[currentUser].title = res.user.occupation;
+                numberInArray ++;
             }
         });
     }
 }
+
 
 // Behance API - List Designer's Projects
 function loadProjects(i, behanceUser) {
@@ -134,14 +160,14 @@ function loadProjects(i, behanceUser) {
         },
         // Ajax request complete
         success: function(res) { // eslint-disable-line
-            // Remove preloader
+            console.log(res);
             setTimeout(function() {
                 $('.pre-loader').detach();
                 $('.projects').empty();
                 var projects = res.projects;
                 if (res.projects.length > 0) {
                     projects.forEach(function(project) {
-                        $(`<li><div class="image-container"><img class="project-thumbnail" src="${project.covers['404']}"></div></li>`).appendTo(`ul.projects${i}`);
+                        $(`<li><div class="image-container"><img class="project-thumbnail" src="${project.covers['404']}"><div class="thumbnail-overlay"><h5 class="thumbnail-title">${project.name}</h5><h5 class="thumbnail-appreciations"><i class="fas fa-thumbs-up"></i> ${project.stats.appreciations}</h5><h5 class="thumbnail-views">${project.stats.views} <i class="fas fa-eye"></i></h5></div></div></li>`).appendTo(`ul.projects${i}`);
                     });
                 } else {
                     $('<div class="no-results"><p>No matching projects.<br>Please adjust your search filters and try again.</p>').prependTo('.projects');
@@ -176,3 +202,4 @@ function loadProjects(i, behanceUser) {
 //     designerCardHeight = $('.designer-card').height();
 //     triggerHeight = designerCardHeight + 75;
 // });
+
